2e4b9d38c19895d48610febef2c8b3cf8c8ba270
aks97cs <anuj.singh@myoperator.co>
2019-10-12 16:04:29 +0530

    update v1

--- mark1/views.py
+++ mark1/views.py
@@ -8,21 +8,32 @@ from django.views.decorators.csrf import csrf_exempt
 @csrf_exempt
 def connectTodb(request):
     data = request.POST
-    print(data)
-    talkTdbServer(data['username'], data['password'], data['host'])
-    dbcur.execute('show databases')
+    talkTdbServer(data['username'], data['password'], data['host'], data['dbname']) if 'dbname' in data else talkTdbServer(data['username'], data['password'], data['host'])
+    sql  = 'show tables' if 'dbname' in data else 'show databases'
+    dbcur.execute(sql)
     dbs = dbcur.fetchall()
     resp = {'status': 'success', 'data': dbs}
     return JsonResponse(resp)
 
 
-def  talkTdbServer(username, password, host):
+def  talkTdbServer(username, password, host, dbname = False):
     try:
         global dbfrom
-        dbfrom = MySQLdb.connect(host, username, password)
+        if not dbname:
+            dbfrom = MySQLdb.connect(host, username, password)
+        else:
+            dbfrom = MySQLdb.connect(host, username, password, dbname)
         print("database connected ..")
         global dbcur
         dbcur = dbfrom.cursor()
     except:
         print("failed")
+    
+@csrf_exempt
+def tableDescribe(request):
+    data = request.POST
+    pass
+
+def talkTotable(data):
+    pass
 
